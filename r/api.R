a6c3ad1c79f69e9990a9c1180211db5d0bda5f2b
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2024-11-05 15:02:06 -0800

    Fix API call for indicators, performance measures, and measures

--- r/api.R
+++ r/api.R
@@ -1,4 +1,7 @@
-sc_read_objects <- function(objects) {
+sc_read_objects <- function(objects, container_id = NA) {
+  
+  # objects: 'scorecards','containers', 'indicators', 'perfmeasures', or 'measures'. 'measures' returns both 'indicators' and 'perfmeasures'.
+  # container_id: optional for indicators, perfmeaures, or measures if you want to limit the list to those in a specific container
   
   tryCatch({
     
@@ -6,11 +9,29 @@ sc_read_objects <- function(objects) {
       stop("'objects' argument must be: 'scorecards','containers', 'indicators', 'perfmeasures', or 'measures'")
     }
     
-    paste0("https://api.resultsscorecard.com/api/", objects ,"/list") %>%
+    
+    if (objects %in% c("indicators", "perfmeasures", "measures")) {
+      url <- "https://api.resultsscorecard.com/api/measures/list"
+      
+      measure_type <- switch(
+        objects,
+        indicators = "indicator", 
+        perfmeasures = "performance measure",
+        NA)
+      
+    } else {
+      url <- paste0("https://api.resultsscorecard.com/api/", objects ,"/list")
+      
+      measure_type <- NA
+    }
+    
+    
+    url %>%
       request() %>%
       req_body_json(list(siteCode = "Portland",
                          apiKey = Sys.getenv("SC_API_KEY"),
-                         measureType = "performance measure"),
+                         measureType = measure_type,
+                         containerID = container_id),
                     encode = "json") %>%
       req_perform() %>%
       resp_body_string() %>%
