7d20b0693dbb33652f10d71a3b3955751b99942d
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2024-11-21 14:59:32 -0800

    Add sc_create_measure

--- r/api.R
+++ r/api.R
@@ -45,3 +45,59 @@ sc_read_objects <- function(objects, container_id = NA) {
   }
   )
 }
+
+
+
+sc_create_measure <- function(
+    measure_type, title, polarity = 1, scale = 0,
+    unit, unit_prefix = FALSE) {
+  
+  # measure_type: string, 'indicators' or 'perfmeasures'
+  # title: string, a name for the measure
+  # polarity: numeric, defaults to 1. 1 - Higher is Better, 2 - Lower is Better, 3 - Target Range, 4 - Do Not Display,5 - No Polarity
+  # scale: numeric, defaults to 0. Number of decimal points to show.
+  # unit: string, defaults to blank. Symbol for the unit of measure, typically % or $.
+  # unit_prefix: boolean, defaults to FALSE. TRUE moves the unit of measure to the front of the number, like "$1". FALSE keeps the unit symbol behind the number, like "1%". 
+  # container_id: optional for indicators, perfmeaures, or measures if you want to limit the list to those in a specific container
+  
+  is.tr
+  
+  tryCatch({
+    
+    if (is.null(measure_type) || !measure_type %in% c("indicators", "perfmeasures")) {
+      stop("'measure_type' argument must be: 'indicators' or 'perfmeasures'")
+    }
+    
+    if (is.null(title) || !is.character(title)) {
+      stop("'title' argument is required and must be a string")
+    }
+
+    url %>%
+      request("https://api.resultsscorecard.com/api/measures/list") %>%
+      req_body_json(list(siteCode = "Portland",
+                         apiKey = Sys.getenv("SC_API_KEY"),
+                         measureType = measure_type,
+                         # containerID = container_id,
+                         calculationTypeId: 1,
+                         calendarId: 1,
+                         colorBands: FALSE,
+                         isBestPractice: FALSE,
+                         polarityId: 1,
+                         scale: 1,
+                         title: title,
+                         measureType: measure_type,
+                         unitOfMeasure: unit,
+                         uoMisPrefix: unit_prefix),
+                    encode = "json") %>%
+      req_perform() %>%
+      resp_body_string() %>%
+      fromJSON()
+  },
+  
+  error = function(cond) {
+    
+    warning("Error creating ", measure_type, ": ", title ," in Scorecard: ", cond)
+    
+  }
+  )
+}
