1182538456c2aa329482d0ac357e31428190ba39
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2024-11-21 15:20:58 -0800

    Update sc_create_measure and sc_read_objects to send API requests to test environment by default

--- r/api.R
+++ r/api.R
@@ -1,7 +1,8 @@
-sc_read_objects <- function(objects, container_id = NA) {
+sc_read_objects <- function(objects, container_id = NA, test = TRUE) {
   
   # objects: 'scorecards','containers', 'indicators', 'perfmeasures', or 'measures'. 'measures' returns both 'indicators' and 'perfmeasures'.
   # container_id: optional for indicators, perfmeaures, or measures if you want to limit the list to those in a specific container
+  # test_env: boolean, defaults to TRUE. TRUE sends the API request to the test environment (accessed at https://scorecard-test.clearimpact.com), whereas FALSE sends it to production.
   
   tryCatch({
     
@@ -9,10 +10,11 @@ sc_read_objects <- function(objects, container_id = NA) {
       stop("'objects' argument must be: 'scorecards','containers', 'indicators', 'perfmeasures', or 'measures'")
     }
     
-    
     if (objects %in% c("indicators", "perfmeasures", "measures")) {
-      url <- "https://api.resultsscorecard.com/api/measures/list"
-      
+      url <- ifelse(test_env,
+                    "https://api-test.clearimpact.com/api/measures/list",
+                    "https://api.resultsscorecard.com/api/measures/list")
+
       measure_type <- switch(
         objects,
         indicators = "indicator", 
@@ -20,8 +22,11 @@ sc_read_objects <- function(objects, container_id = NA) {
         NA)
       
     } else {
-      url <- paste0("https://api.resultsscorecard.com/api/", objects ,"/list")
-      
+
+      url <- ifelse(test_env,
+                    paste0("https://api-test.clearimpact.com/api/", objects ,"/list"),
+                    paste0("https://api.resultsscorecard.com/api/", objects ,"/list"))
+
       measure_type <- NA
     }
     
@@ -50,7 +55,7 @@ sc_read_objects <- function(objects, container_id = NA) {
 
 sc_create_measure <- function(
     measure_type, title, polarity = 1, scale = 0,
-    unit, unit_prefix = FALSE) {
+    unit, unit_prefix = FALSE, test_env = TRUE) {
   
   # measure_type: string, 'indicators' or 'perfmeasures'
   # title: string, a name for the measure
@@ -59,8 +64,7 @@ sc_create_measure <- function(
   # unit: string, defaults to blank. Symbol for the unit of measure, typically % or $.
   # unit_prefix: boolean, defaults to FALSE. TRUE moves the unit of measure to the front of the number, like "$1". FALSE keeps the unit symbol behind the number, like "1%". 
   # container_id: optional for indicators, perfmeaures, or measures if you want to limit the list to those in a specific container
-  
-  is.tr
+  # test_env: boolean, defaults to TRUE. TRUE sends the API request to the test environment (accessed at https://scorecard-test.clearimpact.com), whereas FALSE sends it to production.
   
   tryCatch({
     
@@ -71,9 +75,13 @@ sc_create_measure <- function(
     if (is.null(title) || !is.character(title)) {
       stop("'title' argument is required and must be a string")
     }
+ 
+    url <- ifelse(test_env,
+                  "https://api-test.clearimpact.com/api/measures/add",
+                  "https://api.resultsscorecard.com/api/measures/add")
 
     url %>%
-      request("https://api.resultsscorecard.com/api/measures/list") %>%
+      request() %>%
       req_body_json(list(siteCode = "Portland",
                          apiKey = Sys.getenv("SC_API_KEY"),
                          measureType = measure_type,
