21ba2308a9905452bc65eb52d1160c3a94030194
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2025-03-25 12:42:40 -0700

    Restructured to allow for combining revised and adopted budget data

--- r/budget_data.R
+++ r/budget_data.R
@@ -16,17 +16,13 @@ data <- readxl::read_xlsx("inputs/AdHoc-FY17to25-2025.01.22.xlsx", "AdHoc-FY17to
 df <- data %>%
   filter(`Account Name` == params$include$account,
          `Bureau Name` != params$exclude$bureau,
-         `Major Object - Name` %in% params$include$object)
-
-citywide <- df %>%
-  group_by(`NEW Service Area - Name`, `Bureau Name`) %>%
+         `Major Object - Name` %in% params$include$object) %>%
+  group_by(`Service Area` = `NEW Service Area - Name`, `Bureau Name`) %>%
   summarise(across(starts_with("FY20"), ~ sum(.x, na.rm = TRUE))) %>%
   pivot_longer(starts_with("FY20")) %>%
-  separate(name, into = c("FY", "Budget Type"), " ", extra = "merge") %>%
+  separate(name, into = c("FY", "Type"), " ", extra = "merge") %>%
   mutate(
-    `Budget Type` = case_when(
-      `Budget Type` == "Adopted Adopted Budget" ~ "Adopted Budget",
-      `Budget Type` == "Revised Revised Budget" ~ "Revised Budget",
-      grepl("Actuals", `Budget Type`) ~ "Actuals",
-      .default = `Budget Type`)
-  )
+    `Budget Type` =
+      case_when(grepl("Revised", Type) ~ "Revised",
+                grepl("Adopted", Type) ~ "Adopted"),
+    Type = ifelse(is.na(`Budget Type`), "Actuals", "Budget"))
