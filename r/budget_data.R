f4d1cb060247f564733bc6f18ff2b4cf95d6ba6f
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2025-03-25 12:03:06 -0700

    Clean and restructured budget data to long format

--- r/budget_data.R
+++ r/budget_data.R
@@ -4,15 +4,29 @@ params <- list(
   object = c("EMS - External Materials and Services",
              "IMS - Internal Materials and Services",
              "PERSONAL - Personnel")
-  )
+)
 
 ---
-
+  
 library(tidyverse)
 library(readxl)
 
 data <- readxl::read_xlsx("inputs/AdHoc-FY17to25-2025.01.22.xlsx", "AdHoc-FY17to25-2025.01.22")
 
-`Account Name` = ""
-`Bureau Name`
-`Major Object - Name`
+df <- data %>%
+  filter(`Account Name` == params$include$account,
+         `Bureau Name` != params$exclude$bureau,
+         `Major Object - Name` %in% params$include$object)
+
+citywide <- df %>%
+  group_by(`NEW Service Area - Name`, `Bureau Name`) %>%
+  summarise(across(starts_with("FY20"), ~ sum(.x, na.rm = TRUE))) %>%
+  pivot_longer(starts_with("FY20")) %>%
+  separate(name, into = c("FY", "Budget Type"), " ", extra = "merge") %>%
+  mutate(
+    `Budget Type` = case_when(
+      `Budget Type` == "Adopted Adopted Budget" ~ "Adopted Budget",
+      `Budget Type` == "Revised Revised Budget" ~ "Revised Budget",
+      grepl("Actuals", `Budget Type`) ~ "Actuals",
+      .default = `Budget Type`)
+  )
