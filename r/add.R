ae43449630194a4f06112ac36a13b164d325402a
lillian.nguyen@portlandoregon.gov <lillian.nguyen@portlandoregon.gov>
2025-11-12 12:43:53 -0800

    Add intermediary functions that add containers/measures based on bureau data

---
+++ r/add.R
@@ -0,0 +1,110 @@
+add_scorecard_bureau <- function(bureau, service_area) {
+  if(!is.na(service_area)) {
+    service_area_sc_id <- sc$scorecards %>%
+      filter(title == service_area) %>%
+      pull(id)
+  }
+  
+  sc_add_scorecard(title = bureau, parent_id = service_area_sc_id,
+                   description = NA, test_env = FALSE) 
+}
+
+add_prog_offers <- function(bureau) {
+  po <- prog_offers %>%
+    filter(`Bureau Name` == bureau) %>%
+    pull(`Program Offer`)
+  
+  scorecard_id <- sc$scorecards %>%
+    filter(title == bureau) %>%
+    pull(id)
+  
+  if (is.null(scorecard_id) | rlang::is_empty(scorecard_id)) {
+    stop("No parent scorecard found for ", bureau)
+  }
+  
+  for (i in po) {
+    sc_add_container(
+      type = "program offer", title = i, scorecard_id = scorecard_id, test_env = FALSE)
+  }
+}
+
+add_bureau <- function(bureau) {
+  
+  scorecard_id <- sc$scorecards %>%
+    filter(title == bureau) %>%
+    pull(id)
+  
+  if (is.null(scorecard_id)) {
+    stop("No parent scorecard found for ", bureau)
+  }
+  
+  sc_add_container(type = "bureau", title = bureau,  scorecard_id = scorecard_id, test_env = FALSE)
+  
+  message(bureau, " created in Scorecard")
+}
+
+add_measures <- function(bureau, rows = NA) {
+  
+  df <- read_xlsx(paste0("inputs/bfm_metrics_to_transfer/", bureau, " - Performance Template.xlsx")) %>%
+    filter(`SC-Keep Measure?` %in% c("Yes", "New"))
+  
+  # match to SC container IDs, assign bureau if no PO chosen
+  bureau_container_id <- sc$containers %>%
+    filter(containerType == "Bureau",
+           title ==  bureau) %>%
+    pull(id)
+  
+  df <- df %>%
+    mutate(
+      `Container Type` = ifelse(
+        is.na(`SC-Prog Offer`) & is.na(`Program Offer`),
+        "Bureau", "Program Offer"),
+      Container = case_when(
+        `Container Type` == "Bureau" ~ bureau,
+        !is.na(`SC-Prog Offer`) ~ `SC-Prog Offer`,
+        is.na(`SC-Prog Offer`) ~ `Program Offer`),
+      `PO ID` = ifelse(`Container Type` != "Bureau", str_sub(Container, 1, 6), NA)
+    )
+  
+  pos <- sc$containers %>%
+    filter(containerType == "Program Offer") %>%
+    mutate(`PO ID` = str_sub(title, start = -6)) %>%
+    select(id, `PO ID`)
+  
+  df <- df %>%
+    left_join(pos, "PO ID") %>%
+    select(-`PO ID`) %>%
+    rename(`Container ID` = id)  %>%
+    mutate(`Container ID` = ifelse(
+      `Container Type` == "Bureau",
+      bureau_container_id, `Container ID`))
+  
+  if (anyNA(test$`Container ID`)) {
+    stop("No Scorecard container found for metrics in ", bureau)
+  }
+  
+  if (!is.na(rows)) {
+    df <- slice(df, rows)
+  }
+  
+  for (i in 1:nrow(df)) {
+    
+    metric <- df[i,]
+    
+    sc_add_measure(
+      measure_type = "performance measure",
+      title = metric$`Measure Title`,
+      container_id = po_container_id,
+      calendar = "fy",
+      data_source = metric$`SC-Data Source`,
+      polarity = str_trunc(metric$`SC-Polarity`, 1, ellipsis = ""),
+      scale = metric$`SC-Decimals`,
+      unit = metric$`SC-Unit of Measure`,
+      unit_prefix = ifelse(
+        !is.na(metric$`SC-Unit of Measure`) &
+          metric$`SC-Unit of Measure` == "$", TRUE, FALSE),
+      test_env = FALSE)
+    
+    message(bureau, ": Added ", metric$`Measure Title`, " metric")
+  }
+}
