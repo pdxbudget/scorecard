74cf98977ab96c12aa5c167ca0c75ad2319b726c
Nguyen <Lillian.Nguyen@portlandoregon.gov>
2025-11-24 16:58:43 -0800

    Add default settings for add_measure parameters based on template responses

--- r/add.R
+++ r/add.R
@@ -43,7 +43,7 @@ add_bureau <- function(bureau) {
   message(bureau, " created in Scorecard")
 }
 
-add_measures <- function(bureau, rows = NA) {
+add_measures <- function(bureau, rows = NULL) {
   
   df <- read_xlsx(paste0("inputs/bfm_metrics_to_transfer/", bureau, " - Performance Template.xlsx")) %>%
     filter(`SC-Keep Measure?` %in% c("Yes", "New"))
@@ -74,37 +74,68 @@ add_measures <- function(bureau, rows = NA) {
   df <- df %>%
     left_join(pos, "PO ID") %>%
     select(-`PO ID`) %>%
-    rename(`Container ID` = id)  %>%
-    mutate(`Container ID` = ifelse(
+    rename(`Final Container ID` = id)  %>%
+    mutate(`Final Container ID` = ifelse(
       `Container Type` == "Bureau",
-      bureau_container_id, `Container ID`))
+      bureau_container_id, `Final Container ID`))
   
-  if (anyNA(test$`Container ID`)) {
+  if (anyNA(df$`Final Container ID`)) {
     stop("No Scorecard container found for metrics in ", bureau)
   }
   
-  if (!is.na(rows)) {
-    df <- slice(df, rows)
-  }
+  # defaults
   
+  df <- df %>%
+    mutate(
+      # metric title defaults to BFM title
+      `Final Title` = 
+        ifelse(is.na(`SC-Title`),
+               `Measure Title`, `SC-Title`),
+      # frequency default to FY
+      # performance template incorrectly switched the FY and FY QT IDs, so this is corrected here
+      `Final Frequency` = 
+        case_when(`SC-Frequency` == "7403 - Fiscal Year" ~ "7300 - Fiscal Year",
+                  `SC-Frequency` == "7300 - Fiscal Year - Quarter" ~ "7403 - Fiscal Year - Quarter",
+                  is.na(`SC-Frequency`) ~ "7300 - Fiscal Year",
+                  .default = `SC-Frequency`),
+      # data source is kept as-is, with NAs displaying as blank
+      # polarity default to Do Not Display
+      `Final Polarity` =  
+        case_when(
+          is.na(`SC-Polarity`) & `Desired Direction` %in% c("UP", "UPWARD") ~ "1 - Higher is Better",
+          is.na(`SC-Polarity`) & `Desired Direction` == "DOWN" ~ "2 - Lower is Better",
+          is.na(`SC-Polarity`) & `Desired Direction` %in% c("0", "NONE", "NA") ~ "5 - No Polarity",
+          .default = `SC-Polarity`,
+        ),
+      # decimals default to 0
+      `Final Decimals` = 
+        ifelse(is.na(`SC-Decimals`),
+               0, `SC-Decimals`)
+      # Unit of Measure is kept as-is, with NA showing as a count or sum
+      )
+
+  # if (!is.na(rows)) {
+  #   df <- slice(df, rows)
+  # }
+
   for (i in 1:nrow(df)) {
-    
+
     metric <- df[i,]
-    
+
     sc_add_measure(
       measure_type = "performance measure",
-      title = metric$`Measure Title`,
-      container_id = po_container_id,
-      calendar = "fy",
+      title = metric$`Final Title`,
+      container_id = metric$`Final Container ID`,
+      calendar = str_extract(metric$`Final Frequency`, "[0-9]{1,4}"),
       data_source = metric$`SC-Data Source`,
-      polarity = str_trunc(metric$`SC-Polarity`, 1, ellipsis = ""),
-      scale = metric$`SC-Decimals`,
+      polarity = str_sub(metric$`Final Polarity`, 1, 1),
+      scale = metric$`Final Decimals`,
       unit = metric$`SC-Unit of Measure`,
       unit_prefix = ifelse(
         !is.na(metric$`SC-Unit of Measure`) &
           metric$`SC-Unit of Measure` == "$", TRUE, FALSE),
       test_env = FALSE)
-    
+
     message(bureau, ": Added ", metric$`Measure Title`, " metric")
   }
 }
